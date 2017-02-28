72913d8811139234b36a5603477e32bc880135aa
balpino <balpino>
2017-02-28 18:22:29 -0300

    Lambda

---
+++ Lambda/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lambda
+{
+
+    class Program
+    {
+        delegate int Divide(int a, int b);
+        static void Main(string[] args)
+        {
+            Divide d = (x,y) => x / y;
+            //Console.WriteLine(d(10, 5));
+
+            IEnumerable<string> itens = new List<string>(){ "tenis", "sapato", "meia", "chuteira", "meia" };
+
+            List<string> result = itens.Where(i => i == "sapato").ToList();
+            foreach (string s in result)
+            {
+                Console.WriteLine(s.ToString());
+            }
+            
+            Console.ReadKey();
+        }
+    }
+}
